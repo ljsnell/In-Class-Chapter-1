ea474286a5895b36276ca1b4614a298a5e897f2f
ljsnell <ljsnell7@gmail.com>
2022-08-25 20:38:05 -0500

    Create Hellov2.java
    My first println :)

---
+++ inclass/src/inclass/Hellov2.java
@@ -0,0 +1,10 @@
+package inclass;
+
+public class Hellov2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Java Application");
+	}
+
+}
