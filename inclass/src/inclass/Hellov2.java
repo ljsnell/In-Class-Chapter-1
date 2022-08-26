6be9c048fcc4968cd5dbb282ade82cc39e553dd2
ljsnell <ljsnell7@gmail.com>
2022-08-25 20:47:18 -0500

    Update Hellov2.java
    Not a dialog box :/

--- inclass/src/inclass/Hellov2.java
+++ inclass/src/inclass/Hellov2.java
@@ -4,7 +4,7 @@ public class Hellov2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("First Java Application");
+		System.out.println("First Java Application8-46");
 	}
 
 }
